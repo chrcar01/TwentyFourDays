d83e80cbc1f557efdecca41172a9bc45610df8cb
chrcar01 <chrisjcarter@gmail.com>
2020-11-26 13:42:17 -0800

    placeholder project for folder structure

---
+++ src/2020/Day1/Day1Placeholder/Day1Placeholder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day1Placeholder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
